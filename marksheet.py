ba1bcaa3f6d0ab9513d822a3fbf63f1f557ca095
RameshMagar <you@example.com>
2018-08-13 16:11:46 +0545

    first commit

---
+++ marksheet.py
@@ -0,0 +1,47 @@
+class Marksheet:
+    subjectList = []
+    
+    def addMarksAndSubject(self, subject, mark):
+
+        for i in range(len(subject)):
+            sub = {'subName': subject[i], 'mark': mark[i]}
+            self.subjectList.append(sub)
+    
+    def calculatePercentage(self):
+
+        result = 0
+        for i in range(len(self.subjectList)):
+            n = int(self.subjectList[i]['mark'])
+            result += n
+        percentage = result/len(self.subjectList)
+        return percentage    
+        
+    def findDivision(self):
+        #calckate the division on the basis of the percentage
+        percentage = self.calculatePercentage()
+        if percentage > 80:
+            print("Congratulations! You got Distinction.")
+        elif percentage < 80 and percentage > 60:
+            print("Congratulations! You got First Division.")
+        elif percentage < 60 and percentage > 50:
+            print("Congratulations! You got Second Division.")
+        elif percentage > 50:
+            print("Congratulations! You got Third Division.")
+        else:
+            print("Sorry! No Division. Better luck next time.")
+
+
+subject = []
+mark = []
+n = int(input("how many subjects?"))
+for i in range(n):
+    s = input("enter subject name: ")
+    m = input("enter marks obtained: ")
+    subject.append(s)
+    mark.append(m)
+
+std = Marksheet()
+std.addMarksAndSubject(subject, mark)
+p = std.calculatePercentage()
+print("You have got {} %".format(p))
+std.findDivision()
---
+++ student.py
@@ -0,0 +1,27 @@
+
+from book import Book
+class Student:
+    
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+    
+    def getName(self):
+        return self.name
+    
+    def getAddress(self):
+        return self.address
+
+n = int(input("Enter number of students"))
+student = []
+
+for i in range(n):
+    x = input("Enter name: ")
+    y = input("Enter address: ")
+    stud = Student(x, y)
+    student.append(stud)
+boo=Book("02","Romeo & Juliet","William Shakespear","3rd")
+disp = map(lambda stud: stud.getName() + " " + stud.getAddress(), student)
+print(list(disp))
+boo.getAuthor()
+print(boo.getAuthor())
